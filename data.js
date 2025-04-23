859c7900ae74c9e4ec79981033bdc3816969681b
Disha Gupta <dishagupta.9122002@gmail.com>
2025-04-23 21:09:21 +0530

    updat

---
+++ data.js
@@ -0,0 +1,7 @@
+const data=[
+    {name:"disha",email:"abc@gamil.com",phone:93241673},
+    {name:"mani",email:"abc@gamil.com",phone:93241673},
+    {name:"paras",email:"abc@gamil.com",phone:93241673},
+    {name:"sandhaya",email:"abc@gamil.com",phone:93241673}
+]
+module.exports=data;
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const http=require('http');
-
+const color=require('colors');
+const data = require('./data')
 http.createServer((res,resp)=>{
-    resp.write('hii guys , I am learing node js');
+    resp.writeHead(200,{'content-type':'application/json'});
+    resp.write(JSON.stringify(data));
     resp.end();
-}).listen(4500);
+
+}).listen(1500);
+
+console.log("hiii")
+
+console.log("step by step command line input")
